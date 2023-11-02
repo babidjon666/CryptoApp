5dae4be894d98112798d5c416721d62fcdfe7c04
MarkPolyakov <markpolya@icloud.com>
2023-11-02 17:31:58 +0500

    add hex color extension

---
+++ CryptoApp/Models/CryptoModel.swift
@@ -0,0 +1,8 @@
+//
+//  CryptoModel.swift
+//  CryptoApp
+//
+//  Created by Марк Поляков on 02.11.2023.
+//
+
+import Foundation
---
+++ CryptoApp/Views/PortfolioView.swift
@@ -0,0 +1,18 @@
+//
+//  PortfolioView.swift
+//  CryptoApp
+//
+//  Created by Марк Поляков on 02.11.2023.
+//
+
+import SwiftUI
+
+struct PortfolioView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PortfolioView()
+}
