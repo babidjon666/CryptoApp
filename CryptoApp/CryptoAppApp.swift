af65b9a883c1c787fbc522fbdbf941d8ee3d6dde
MarkPolyakov <markpolya@icloud.com>
2023-11-02 17:19:56 +0500

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Марк Поляков on 02.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
